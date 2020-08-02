7907528f0b6e9e1a1c2a2d349fb3c239e5972408
Menghorng96 <61418126+Menghorng96@users.noreply.github.com>
2020-08-02 11:03:40 +0700

    Add files via upload

---
+++ src/sawyer_robot/scripts/position_control.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Float64
+
+def position_joint (j0, j1, j2, j3, j4, j5, j6):
+     publisher_j0.publish(j0)
+     publisher_j1.publish(j1)
+     publisher_j2.publish(j2)
+     publisher_j3.publish(j3)
+     publisher_j4.publish(j4)
+     publisher_j5.publish(j5)
+     publisher_j6.publish(j6)
+
+if __name__ == "__main__":
+
+     rospy.init_node("test", anonymous=True)
+     
+     publisher_h = rospy.Publisher('/sawyer/head_pan_position_controller/command',Float64, queue_size=1.0)
+     publisher_j0 = rospy.Publisher('/sawyer/right_j0_position_controller/command',Float64, queue_size=1.0)
+     publisher_j1 = rospy.Publisher('/sawyer/right_j1_position_controller/command',Float64, queue_size=1.0)
+     publisher_j2 = rospy.Publisher('/sawyer/right_j2_position_controller/command',Float64, queue_size=1.0)
+     publisher_j3 = rospy.Publisher('/sawyer/right_j3_position_controller/command',Float64, queue_size=1.0)
+     publisher_j4 = rospy.Publisher('/sawyer/right_j4_position_controller/command',Float64, queue_size=1.0)
+     publisher_j5 = rospy.Publisher('/sawyer/right_j5_position_controller/command',Float64, queue_size=1.0)
+     publisher_j6 = rospy.Publisher('/sawyer/right_j6_position_controller/command',Float64, queue_size=1.0)
+     
+     while not rospy.is_shutdown():
+        try:
+           #j6 = input("J6: ")
+           #publisher_h.publish(0.50)
+           #neutral: [0.00, -1.18, 0.00, 2.18, 0.00, 0.57, 3.3161]
+           #shipping: [0.00, -1.57, 0.00, 2.79, 0.00, -2.79, 3.3161]
+           position_joint(0.00, -1.18, 0.00, 2.7, 1.5, 0.0, 3.2)  
+           #position_joint(0.00, 0.00, 0.00, 0.00, 0.00, 0.59, 0.00) 
+
+        except KeyboardInterrupt: 
+            print "Ctrl+c"
+            break
